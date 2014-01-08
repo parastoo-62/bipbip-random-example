4a87af04dcee9807e0dcdabead927310f5462b58
Reto Kaiser <reto@cargomedia.ch>
2014-01-08 21:55:31 +0100

    Add example plugin, measuring random numbers

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-bipbip-exampleplugin
-====================
+bipbip-example
+=========
+Plugin example for [bipbip](https://github.com/cargomedia/bipbip).
 
-Plugin example for https://github.com/cargomedia/bipbip
+Configuration
+-------------
+```yml
+plugin: example
+max: 10
+```
---
+++ lib/bipbip/plugin/example.rb
@@ -0,0 +1,19 @@
+module Bipbip
+
+  class Plugin::Example < Plugin
+
+    def metrics_schema
+      [
+          {:name => 'random_1', :type => 'gauge', :unit => 'Number'},
+          {:name => 'random_2', :type => 'gauge', :unit => 'Number'},
+      ]
+    end
+
+    def monitor
+      random_1 = Random.rand(config['max'])
+      random_2 = Random.rand(config['max'])
+
+      {'random_1' => random_1, 'random_2' => random_2}
+    end
+  end
+end
